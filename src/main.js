9fb2c86474d90f863c9d25f4d85ee964376d0a2b
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-10-20 13:18:16 +0200

    Moves some files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // https://getbootstrap.com/docs/5.2/getting-started/vite/
 
 // Import our custom CSS
-import '../scss/styles.scss'
+import './scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
